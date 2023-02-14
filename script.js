501f465bb7e14952f36c451246f5b55a8802b637
Damir <damir24may@mail.ru>
2023-02-14 03:45:19 +0300

    add attemps to lesson06

--- script.js
+++ script.js
@@ -10,36 +10,45 @@ const isNum = function (n) {
 
 const begin = function () {
   let randomNum = getRandomInt(100);
+  let attemps = 10;
   const game = function () {
-    const num = prompt("Угадай число от 1 до 100");
-    if (num === null) {
-      alert("До свидания");
-      return;
-    }
-    if (isNum(num)) {
-      const realNum = +num;
-      if (realNum > randomNum) {
-        alert("Загаданное число меньше");
-        game();
-      } else if (realNum > randomNum) {
-        alert("Загаданное число больше");
-        game();
+    attemps--;
+    if (attemps <= 0) {
+      if (confirm("Попытки закончились, хотите сыграть еще ?")) {
+        begin();
       } else {
-        if (confirm("Поздравляю, вы угадали!")) {
-          begin();
-        } else {
-          alert("До свидания");
-          return;
-        }
+        alert("До свидания");
+        return;
       }
     } else {
-      alert("Введите число");
-      game();
+      const num = prompt("Угадай число от 1 до 100");
+      if (num === null) {
+        alert("До свидания");
+        return;
+      }
+      if (isNum(num)) {
+        const realNum = +num;
+        if (realNum > randomNum) {
+          alert("Загаданное число меньше, осталось попыток: " + attemps);
+          game();
+        } else if (realNum > randomNum) {
+          alert("Загаданное число больше, осталось попыток: " + attemps);
+          game();
+        } else {
+          if (confirm("Поздравляю, вы угадали!")) {
+            begin();
+          } else {
+            alert("До свидания");
+            return;
+          }
+        }
+      } else {
+        alert("Введите число");
+        game();
+      }
     }
   };
   game();
 };
 
 begin();
-
-console.log(randomNum);
